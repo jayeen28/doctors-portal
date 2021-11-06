7b5aa078d8bf720035fc65c6af3f75b167368fc7
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-06 19:08:47 +0600

    appointment page booking data changed

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -5,39 +5,39 @@ const bookings = [
     {
         id: 1,
         name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
     },
     {
         id: 2,
-        name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
+        name: 'Cosmetic Dentistry',
+        time: '09.00 AM - 10.00 AM',
+        space: 8,
     },
     {
         id: 3,
-        name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
+        name: 'Teeth Cleaning',
+        time: '10.00 AM - 11.00 AM',
+        space: 9,
     },
     {
         id: 4,
-        name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
+        name: 'Cavity Protection',
+        time: '11.00 AM - 12.00 PM',
+        space: 5,
     },
     {
         id: 5,
-        name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
+        name: 'Pediatric Dental',
+        time: '06.00 PM - 07.00 PM',
+        space: 10,
     },
     {
         id: 6,
-        name: 'Teeth Orthodonics',
-        time: '8.00 AM - 9.00 AM',
-        space: 10
-    }
+        name: 'Oral Surgery',
+        time: '07.00 PM - 08.00 PM',
+        space: 10,
+    },
 ]
 const AvailableAppointment = ({ date }) => {
     return (
